eb97a3c1b26357f1b64c6173607291d410964e9e
salamer <salamer_gaga@163.com>
2024-09-17 22:27:39 +0800

    fix

--- build.sh
+++ build.sh
@@ -10,4 +10,5 @@ apt-get autoremove -y
 apt-get clean -y
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 pip install flask gunicorn pillow requests
+pip install easyocr
 python load.py
