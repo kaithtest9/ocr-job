c472e0b8739576a70df4a44f4c26a45a20ec5e30
salamer <salamer_gaga@163.com>
2024-09-17 22:15:18 +0800

    fix

--- build.sh
+++ build.sh
@@ -5,11 +5,9 @@ apt-get update -y && \
     libxext6 \
     libxrender-dev \
     libgl1-mesa-dev \
-    git \
-    # cleanup
-    && apt-get autoremove -y \
-    && apt-get clean -y \
-    && rm -rf /var/lib/apt/lists
+    git
+apt-get autoremove -y
+apt-get clean -y
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 pip install flask gunicorn pillow requests
 python load.py
